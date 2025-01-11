fb98d9f2bcb410dab60a9a1df841bd4e889dfb00
Emon <emonhasan7650@gmail.com>
2025-01-11 22:46:03 +0600

    some backend isuue solved

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,27 @@ import router from './app/routes';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
 import cookieParser from 'cookie-parser';
-import config from './app/config';
 const app: Application = express();
 
 //* parser
 app.use(express.json());
-app.use(cors({origin: config.client_url, credentials: true}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://emon-hassan-portfolio.vercel.app',
+    ],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 // application routes
 app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Portfolio app server is running!')
-})
+  res.send('Portfolio app server is running!');
+});
 
 // use global error handler middleware
 app.use(globalErrorHandler);
